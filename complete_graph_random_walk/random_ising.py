6fd490cf1a70266ddef7af1f0518f283be3437fa
Sami <sami_b95@hotmail.fr>
2019-09-21 12:44:30 +0200

    Added random Ising (max-cut) generator with demo

--- complete_graph_random_walk/random_ising.py
+++ complete_graph_random_walk/random_ising.py
@@ -1,11 +1,9 @@
-import sys
-sys.path.append(".")
 import graph
 
 def random_max_cut_ising(n):
     if n % 1:
         raise Exception("number of vertices must be even")
-    g = random_d_regular_graph(n, 3)
+    g = graph.random_d_regular_graph(n, 3)
     edges = {}
     for v in g:
         for adj_v in g[v]:
